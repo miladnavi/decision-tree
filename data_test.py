a1641390812a30c07bb6c07cf72787f418b0b683
Jonathan Lennartz <jlen@uni-bonn.de>
2019-03-04 02:17:59 +0100

    cross val + time

--- data_test.py
+++ data_test.py
@@ -6,8 +6,8 @@ from classifier_py_file import pre_process
 from forest import Forest
 
 # :-------------------------------------------
-# data = np.array(pd.read_csv('dataset_61_iris.csv'))
-data = np.array(pd.read_csv('dataset_13_breast-cancer.csv'))
+data = np.array(pd.read_csv('dataset_61_iris.csv'))
+# data = np.array(pd.read_csv('dataset_13_breast-cancer.csv'))
 '''
 Two examples. One with the original numerical data from the iris data set. The second one is a discretization of the 
 former without duplicates. Un-comment to use either one.
@@ -16,22 +16,23 @@ former without duplicates. Un-comment to use either one.
 # x_train = np.array(data[:, 0:4])
 # x_train = np.vstack((np.array([1, 1, 1, 1]), x_train))
 # y_train = np.array(data[:, 4])
-
+# data_types = np.array([1, 1, 1, 1])
 
 # :----------- ordinal
-# x_train = np.array(data[:, 0:4])
-# x_train = np.round(x_train.astype(np.double))
+x_train = np.array(data[:, 0:4])
+x_train = np.round(x_train.astype(np.int))
 # x_train = np.vstack((np.array([2, 2, 2, 2]), x_train))
-# y_train = data[:, 4]
+y_train = data[:, 4]
+data_types = np.array([2, 2, 2, 2])
 
 
-x_train = np.array(data[:, 0:9])
-#  x_train = np.vstack((np.array([2, 2, 2, 2, 2, 2, 2, 2, 2]), x_train))
-y_train = data[:, 9]
+# x_train = np.array(data[:, 0:9])
+# x_train = np.vstack((np.array([2, 2, 2, 2, 2, 2, 2, 2, 2]), x_train))
+# y_train = data[:, 9]
 
-x, y = pre_process(x_train[:, 0], y_train, [2])
 
-print(x)
+x, y = pre_process(x_train, y_train, data_types)
+
 
 
 '''
@@ -39,12 +40,12 @@ Initialize the model with a Node from the Node class. Node contains a method to 
 and the corresponding label vector Y_train. Note that X_train contains information about the data type in the first row.
 '''
 # tree = Node()
-# tree.fit(x_train, y_train)
+# tree.fit(x, y, data_types[[0]])
 
 '''
 After fitting, the model can be used to predict the label of a given instance. 
 '''
-# instance = x_train[4, :]
+instance = x_train[4, :]
 # print(tree.predict(instance))
 
 '''
@@ -57,9 +58,9 @@ Access instance attributes
 Decision Forest Test
 '''
 
-# forest = Forest()
-# forest.nurture_forest(x_train, y_train, 5)
-
+forest = Forest()
+forest.fit(x_train, y_train, data_types, 2)
+print(forest.predict(instance))
 
 # print(forest.ask_forest_for_guidance(instance))
 
